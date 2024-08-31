a770aa8f00be688aa9bc3f61e766f5480bb3d7fe
drdaza <drdaza18@gmail.com>
2024-08-31 18:57:27 +0200

    litle changes

--- installJDK.py
+++ installJDK.py
@@ -16,9 +16,9 @@ def install_jdk(platform, jdk_version = "17", maven_version="3.8.8"):
         try:
             if not os.path.exists(f"C:\\Program Files\\Java\\jdk-{jdk_version}"):
                 install_jdk_on_windows(jdk_version)
-            set_environment_variables_on_windows(jdk_version)
             if not os.path.exists(f"C:\\Program Files\\apache-maven\\apache-maven-{maven_version}"): 
                 install_maven_on_windows()
+            set_environment_variables_on_windows(jdk_version, maven_version)
         except Exception as e:
             print(f"An error occurred: {e}")
             sys.exit(1)
@@ -29,18 +29,18 @@ def install_jdk_on_windows(jdk_version):
 
     if not os.path.exists(jdk_file):
         subprocess.run(["curl", "-L", jdk_url, "-o", jdk_file])
-        print(f"JDK descargado: {jdk_file}")
+        print(f"JDK downloaded: {jdk_file}")
     else:
-        print(f"El archivo {jdk_file} ya existe. Saltando descarga.")
+        print(f"the file {jdk_file} all ready exist. Skip download.")
 
 
     try:
         subprocess.run(["start", jdk_file], shell=True)
-        print("Ejecutando instalador...")
+        print("Running installer...")
     except subprocess.CalledProcessError as e:
-        print(f"Error al ejecutar el instalador: {e}")
+        print(f"Error running the installer: {e}")
     except FileNotFoundError:
-        print("El archivo no se encontró o no se puede ejecutar.")
+        print("The file was not found or cannot be executed.")
 
     # jdk_home = os.path.join("C:\\Program Files", f"Java\\jdk-{jdk_version}")
     # os.environ["JAVA_HOME"] = jdk_home
@@ -66,11 +66,12 @@ def install_maven_on_windows(maven_version="3.8.8"):
     if not os.path.exists(maven_file):
         try:
             subprocess.run(["curl", "-L", maven_url, "-o", maven_file])
+            print(f"Maven downloaded: {maven_file}")
         except Exception as e:
             print(f"Error downloading Maven: {e}")
             return
     else:
-        print(f"El archivo {maven_file} ya existe. Saltando descarga.")
+        print(f"the file {maven_file} all ready exist. Skip download.")
 
     
 
@@ -84,21 +85,21 @@ def install_maven_on_windows(maven_version="3.8.8"):
 
     # subprocess.run(["unzip", maven_file, "-d", f"apache-maven-{maven_version}"])
 
-    exixst_maven_home = False
-    if exixst_maven_home == True:
-        maven_home = f"C:\\Program Files\\apache-maven\\apache-maven-{maven_version}"
-        # os.environ["MAVEN_HOME"] = maven_home
-        # os.environ["PATH"] = f"{maven_home}/bin:{os.environ['PATH']}"
-        maven_home_set_command = ["setx", "MAVEN_HOME", maven_home]
-        path_update_command = ["setx", "PATH", f"{maven_home}/bin;%{os.environ['PATH']}"]
+    # exixst_maven_home = False
+    # if exixst_maven_home == True:
+    #     maven_home = f"C:\\Program Files\\apache-maven\\apache-maven-{maven_version}"
+    #     # os.environ["MAVEN_HOME"] = maven_home
+    #     # os.environ["PATH"] = f"{maven_home}/bin:{os.environ['PATH']}"
+    #     maven_home_set_command = ["setx", "MAVEN_HOME", maven_home]
+    #     path_update_command = ["setx", "PATH", f"{maven_home}/bin;%{os.environ['PATH']}"]
 
 
-    try:
-        subprocess.run(maven_home_set_command, check=True)
-        subprocess.run(path_update_command, check=True)
-        print("Variables de entorno establecidas con éxito.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error al establecer las variables de entorno: {e}")
+    # try:
+    #     subprocess.run(maven_home_set_command, check=True)
+    #     subprocess.run(path_update_command, check=True)
+    #     print("Variables de entorno establecidas con éxito.")
+    # except subprocess.CalledProcessError as e:
+    #     print(f"Error al establecer las variables de entorno: {e}")
     # maven_url = "https://dlcdn.apache.org/maven/maven-3.8.7/apache-maven-3.8.7-bin.zip"
     # maven_file = "apache-maven-3.8.7-bin.zip"
     # subprocess.run(["wget", maven_url, "-O", maven_file])
@@ -131,32 +132,37 @@ def set_environment_variables(jdk_version):
     os.environ["PATH"] = f"{jdk_home}/bin:{os.environ['PATH']}"    
 
 
-def set_environment_variables_on_windows(jdk_version): 
+def set_environment_variables_on_windows(jdk_version, maven_version): 
+        # java environment variables config
         jdk_home = f"C:\\Program Files\\Java\\jdk-{jdk_version}"
-        print(jdk_home)
         java_home_set_command = ["setx", "JAVA_HOME", jdk_home]
-        path_update_command = ["setx", "PATH", f"{jdk_home}/bin;%{os.environ['PATH']}"]
-
+        path_update_java_command = ["setx", "PATH", f"{jdk_home}/bin;%{os.environ['PATH']}"]
+        # maven environment variables config
+        maven_home = f"C:\\Program Files\\apache-maven\\apache-maven-{maven_version}"
+        maven_home_set_command = ["setx", "MAVEN_HOME", maven_home]
+        path_update_maven_command = ["setx", "PATH", f"{maven_home}/bin;%{os.environ['PATH']}"]
 
         try:
             subprocess.run(java_home_set_command, check=True)
-            subprocess.run(path_update_command, check=True)
-            print("Variables de entorno establecidas con éxito.")
+            subprocess.run(path_update_java_command, check=True)
+            subprocess.run(maven_home_set_command, check=True)
+            subprocess.run(path_update_maven_command, check=True)
+            print("Environment variables set successfully.")
         except subprocess.CalledProcessError as e:
-            print(f"Error al establecer las variables de entorno: {e}")
+            print(f"Error setting environment variables: {e}")
 
 def extract_zip(zip_file, destination):
     try:
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(destination)
     except zipfile.BadZipfile:
-        print("El archivo ZIP está corrupto.")
+        print("The ZIP file is corrupted.")
     except FileNotFoundError:
-        print("El archivo ZIP o la carpeta de destino no existe.")
+        print("The ZIP file or the destination folder does not exist.")
     except PermissionError:
-        print("No tienes permisos suficientes para descomprimir el archivo.")
+        print("You do not have sufficient permissions to unzip the file.")
     except Exception as e:
-        print(f"Ocurrió un error inesperado: {e}")
+        print(f"An unexpected error occurred: {e}")
 def main():
     platformToLowerCase = platform.system().lower()
     install_jdk(platformToLowerCase)
